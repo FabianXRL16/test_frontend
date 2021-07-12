7d9bb43295f4f158f8dcb8e6237eafc1812bb765
Fabian Pacherres <fabianpacherres@gmail.com>
2021-07-12 01:24:39 -0500

    dynamic floating button

--- src/components/buttons/EditFloating.vue
+++ src/components/buttons/EditFloating.vue
@@ -1,11 +1,14 @@
 <template>
   <button
-    class="bg-yellow-600 transform hover:scale-110
+    @click="ok = !ok"
+    class="transform hover:scale-110
         motion-reduce:transform-none h-16 w-16 rounded-full
         fixed right-4 bottom-6 sm:right-24 sm:bottom-8
         md:right-36 md:bottom-16 outline-none btn flex justify-center items-center"
+    :class="ok ? 'bg-yellow-600' : 'bg-red-600'"
   >
-    <img src="../../assets/edit.svg" class="h-8 w-8" alt="editar" />
+    <img v-if="ok" src="../../assets/edit.svg" class="h-8 w-8" alt="editar" />
+    <img v-else src="../../assets/cancel.svg" class="h-6 w-6" alt="cancelar" />
   </button>
 </template>
 
@@ -14,7 +17,9 @@ export default {
   name: "Edit",
   props: {},
   data() {
-    return {};
+    return {
+      ok: true,
+    };
   },
   methods: {},
 };
